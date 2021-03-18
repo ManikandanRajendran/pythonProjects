9fb9d16db22ef2c551ffa05fdd21c0524f3504e4
ManikandanRajendran <manikandan.rajendran1@centrica.com>
2021-03-18 20:05:15 +0000

    folder structure change

---
+++ VoiceAssistant/main.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(1, 'assistance')
+import voiceAssistance
+
+def start_search():
+    voiceAssistance.initiateProgram()
+
+
+if __name__ == '__main__':
+    start_search()
+    
